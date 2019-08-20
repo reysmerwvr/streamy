8cc14e5beb443777e565a0da790991fbac1f94a3
Reysmer Valle <reysmerwvr@gmail.com>
2019-08-19 21:02:55 -0500

    [In Progress] - Adding Redux Form and React Router

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={StreamList}/>
+        <Route path="/streams/new" exact component={StreamCreate}/>
+        <Route path="/streams/edit" exact component={StreamEdit}/>
+        <Route path="/streams/delete" exact component={StreamDelete}/>
+        <Route path="/streams/show" exact component={StreamShow}/>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends React.Component {
+    
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '650607356665-b7u1jli06bvvij974vs8t2jl456qqt3d.apps.googleusercontent.com',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.onAuthChange(this.auth.isSignedIn.get());
+                this.auth.isSignedIn.listen(this.onAuthChange);
+            });
+        });
+    }
+
+    onAuthChange = isSignedIn => {
+        if (isSignedIn) {
+            this.props.signIn(this.auth.currentUser.get().getId());
+        } else {
+            this.props.signOut();
+        }
+    }
+
+    onSignInClick = () => {
+        this.auth.signIn();
+    }
+
+    onSignOutClick = () => {
+        this.auth.signOut();
+    }
+
+    renderAuthButton() {
+        const { isSignedIn } = this.props;
+        if (isSignedIn === null) {
+            return null;
+        } else if(isSignedIn) {
+            return (
+                <button 
+                    className="ui red google button"
+                    onClick={this.onSignOutClick}
+                >
+                    <i className="google icon" />
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button 
+                    className="ui red google button"
+                    onClick={this.onSignInClick}
+                >
+                    <i className="google icon" />
+                    Sign In With Google
+                </button>
+            );
+        }
+    }
+
+    render () {
+        return (
+            <div>{this.renderAuthButton()}</div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn };
+}
+
+const mapDispatchToProps = {
+    signIn,
+    signOut
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoogleAuth);
