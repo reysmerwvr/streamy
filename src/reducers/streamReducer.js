dc8e1a4362400ac43b6c0054c6e0dd4b8df0694f
Reysmer Valle <rvalle@teravisiontech.com>
2019-08-22 18:31:47 -0500

    [In Progress] - Stream Reducer

---
+++ src/reducers/streamReducer.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return {...state, [action.payload]:action.payload };
+    case CREATE_STREAM:
+      return {...state, [action.payload]:action.payload };
+    case EDIT_STREAM:
+      return {...state, [action.payload]:action.payload };
+    default:
+      break;
+  }
+}
