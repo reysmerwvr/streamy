c4aa18ebb7049eee6eb0f0fb8c413ce659065aa3
Reysmer Valle <rvalle@teravisiontech.com>
2019-08-23 18:53:58 -0500

    [In Progress] - Adding history object

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const StreamEdit = () => {
     return <div>StreamEdit</div>;
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, onwProps) => {
+    return { stream: state.streams[onwProps.match.params.id] };
+}
+
+export default connect(mapStateToProps)(StreamEdit);
